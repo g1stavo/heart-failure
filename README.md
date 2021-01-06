4f7c488e5e018a41837345c15f9411a34436cf63
g1stavo <gustavocsalvador@icloud.com>
2021-01-06 18:33:32 -0300

    refactor: move imports, create funcs, update README

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# heart-failure
+# Heart Failure
+
+Application of ML models Logistic Regression, kNN, Decision Tree, Random Forest and Support Vector Machine using a dataset of patients with heart problems.
+
+Activity from UFSC Machine Learning XXII Summer School.
--- main.py
+++ main.py
@@ -2,6 +2,15 @@
 
 import pandas as pd
 
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.metrics import accuracy_score
+from sklearn.linear_model import LogisticRegression
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.svm import LinearSVC
+
 seed = 666
 
 data = pd.read_csv('heart_failure.csv')
@@ -10,8 +19,6 @@ print(data.head())
 x = data.iloc[:, 0:-1].values # features: from first to next-to-last column
 y = data.iloc[:, -1].values # label: last column
 
-from sklearn.model_selection import train_test_split
-
 x_train, x_test, y_train, y_test = train_test_split(
     x, y, test_size = 1/3, random_state = seed)
 
@@ -19,74 +26,60 @@ x_train_2, x_val, y_train_2, y_val = train_test_split(
     x_train, y_train, test_size = 1/3, random_state = seed)
 
 # feature scaling
-
-from sklearn.preprocessing import MinMaxScaler
-
 scaler = MinMaxScaler()
 x_train_2 = scaler.fit_transform(x_train_2)
 x_val = scaler.transform(x_val)
 
 dic = {} # result dictionary
 
-from sklearn.metrics import accuracy_score
+def logistic_regression(): 
+    lr = LogisticRegression(random_state = seed)
+    lr.fit(x_train_2, y_train_2)
 
-# logistic regression
+    return accuracy_score(y_val, lr.predict(x_val))
+    
+def knn():
+    knn = KNeighborsClassifier(n_neighbors = 7)
+    knn.fit(x_train_2, y_train_2)
 
-from sklearn.linear_model import LogisticRegression
+    return accuracy_score(y_val, knn.predict(x_val))
 
-lr = LogisticRegression(random_state = seed)
-lr.fit(x_train_2, y_train_2)
+def decision_tree(): 
+    tree = DecisionTreeClassifier(max_depth = 4, random_state = seed)
+    tree.fit(x_train_2, y_train_2)
 
-dic["logistic_regression"] = accuracy_score(y_val, lr.predict(x_val))
+    return accuracy_score(y_val, tree.predict(x_val))
 
-# k-nn
+def random_forest():
+    rf = RandomForestClassifier(
+        n_estimators= 100, random_state = seed)
+    rf.fit(x_train_2, y_train_2)
 
-from sklearn.neighbors import KNeighborsClassifier
+    return accuracy_score(y_val, rf.predict(x_val))
 
-knn = KNeighborsClassifier(n_neighbors = 7)
-knn.fit(x_train_2, y_train_2)
+def linear_support_vector_machine():
+    svc = LinearSVC()
 
-dic["knn"] =  accuracy_score(y_val, knn.predict(x_val))
-
-# decision tree
- 
-from sklearn.tree import DecisionTreeClassifier
-
-tree = DecisionTreeClassifier(max_depth = 4, random_state = seed)
-tree.fit(x_train_2, y_train_2)
-
-dic["decision_tree"] = accuracy_score(y_val, tree.predict(x_val))
-
-# random forest
-
-from sklearn.ensemble import RandomForestClassifier
-
-rf = RandomForestClassifier(
-    n_estimators= 100, random_state = seed)
-rf.fit(x_train_2, y_train_2)
-
-dic["random_forest"] = accuracy_score(y_val, rf.predict(x_val))
-
-# linear support vector machine
-
-from sklearn.svm import LinearSVC
-svc = LinearSVC()
-
-svc.fit(x_train_2, y_train_2)
-dic["svc"] = accuracy_score(y_val, svc.predict(x_val))
+    svc.fit(x_train_2, y_train_2)
+    return accuracy_score(y_val, svc.predict(x_val))
 
 # evaluating models
+dic["logistic_regression"] = logistic_regression()
+dic["knn"] = knn()
+dic["decision_tree"] = decision_tree()
+dic["random_forest"] = random_forest()
+dic["svc"] = linear_support_vector_machine()
 
 validation = pd.Series(dic, name="algorithms accuracy")
 print() ; print(validation)
 
-# training final model
-
+# train final model
 x_train = scaler.fit_transform(x_train)
 x_test = scaler.transform(x_test)  
 
+rf = RandomForestClassifier(
+    n_estimators= 100, random_state = seed)
 rf.fit(x_train, y_train)
 
-score = str(accuracy_score(y_test, lr.predict(x_test)))
-
+score = str(accuracy_score(y_test, rf.predict(x_test)))
 print() ; print("final accuracy: ", score)
